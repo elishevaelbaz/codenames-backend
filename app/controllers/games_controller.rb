f175de7edf604dc1f9d1c9b67df897a0104603f1
github username <github email address>
2020-07-07 21:25:59 -0400

    removing extra orange from category array

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -31,7 +31,7 @@ class GamesController < ApplicationController
     def new_round
         @game = Game.create(room_code: params[:room_code])
         @words = Word.limit(25).order("RANDOM()")
-        categories = ["orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "purple", "purple", "purple", "purple", "purple", "purple", "purple", "purple", "bomb", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral"].shuffle
+        categories = ["orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "purple", "purple", "purple", "purple", "purple", "purple", "purple", "purple", "bomb", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral"].shuffle
         @words.each_with_index do |word, index|
             GameWord.create(game_id: @game.id, word_id: word.id, category: categories[index], guessed: false)
         end
