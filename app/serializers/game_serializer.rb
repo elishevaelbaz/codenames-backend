45adfaf9bb7a252e4948db14ba0e11f0db22b1d3
github username <github email address>
2020-07-09 09:55:52 -0400

    removing word info from json response

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
   has_many :game_words
-  has_many :words, through: :game_words
   attributes :id, :room_code, :orange_turn, :orange_words_left, :purple_words_left
 end
