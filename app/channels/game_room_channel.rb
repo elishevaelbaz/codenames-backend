f0249eff2e29b32ef4e93feeb7e0fb970e24d92b
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-09-16 15:02:46 -0400

    add puts to indicate when unsibscribe to channel

--- app/channels/game_room_channel.rb
+++ app/channels/game_room_channel.rb
@@ -1,11 +1,12 @@
 class GameRoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    game_room = Game.find_by(room_code: params[:room_code])
+    # game_room = Game.find_by(room_code: params[:room_code])
     stream_for params[:room_code]
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    puts "UNSUBSCRIBED" * 10
   end
 end
